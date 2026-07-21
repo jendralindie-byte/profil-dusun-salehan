1a43639f663c2a01b945c8be2e0ce5aa4844f5f6
jendralindie-byte <jendralindie@gmail.com>
2026-07-21 10:39:45 +0700

    Update supabase.js

--- Dusun Salehan/js/supabase.js
+++ Dusun Salehan/js/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const supabaseUrl = 'https://vufymausskugbgnrtinx.supabase.co' // Pake URL lu
-const supabaseKey = 'sb_publishable_hbGzKC1A9JcXCMx-nCpZVQ_MT3tJLwT' // Pake anon key lu
+const supabaseUrl = 'https://vufvmausskugbgnrtinx.supabase.co'
+const supabaseKey = 'sb_publishable_hbGzKC1A9IcXCMx-nCpZVQ_MT3tJLwT'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
