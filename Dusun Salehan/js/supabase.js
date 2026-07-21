0a5e993c3cf8d598c2425a6626026d9ef7a0defd
jendralindie-byte <jendralindie@gmail.com>
2026-07-21 10:47:09 +0700

    Update supabase.js

--- Dusun Salehan/js/supabase.js
+++ Dusun Salehan/js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const supabaseUrl = 'https://vufvmausskugbgnrtinx.supabase.co'
-const supabaseKey = 'sb_publishable_hbGzKC1A9IcXCMx-nCpZVQ_MT3tJLwT'
+const supabaseUrl = 'https://vufymausskugbgnrtinx.supabase.co'
+const supabaseKey = 'sb_publishable_hbGzKC1A9JcXCMx-nCpZVQ_MT3tJLwT'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
